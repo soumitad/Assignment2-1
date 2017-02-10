49db2663307013c793b3bc678a028704ee3e118e
Soumita Das <sd655@afsconnect1.njit.edu>
2017-02-10 16:22:11 -0500

    added discount amount & discount price

--- display_discount.php
+++ display_discount.php
@@ -2,8 +2,8 @@
 $product_description =$_POST['product_description']; 
 $retail_price =$_POST['list_price'];
 $discount_rate =$_POST['discount_percent'];
-
-
+$discount_amount =$retail_price * ($discount_rate)/100;
+$discount_price =$retail_price - $discount_amount;
 ?>
 
 <!DOCTYPE html>
@@ -26,10 +26,10 @@ $discount_rate =$_POST['discount_percent'];
         <span><?php echo $discount_rate; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_amount; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_price; ?></span><br>
     </main>
 </body>
 </html>
